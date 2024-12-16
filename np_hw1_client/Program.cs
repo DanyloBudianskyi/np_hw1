a4ff70856496d9e31817b4756176bbdd9d533441
Danil <super.danil501@gmail.com>
2024-12-15 22:07:47 -0700

    Add project files.

---
+++ np_hw1_client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ls1_client
+{
+    public class Client
+    {
+        private static TcpClient client { get; set; } = new TcpClient();
+        private static NetworkStream stream { get; set; } = null;
+        private static string Username { get; set; }
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            Username = Console.ReadLine();
+            client = new TcpClient("127.0.0.1", 5000);
+            stream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(Username);
+            await stream.WriteAsync(buffer, 0, buffer.Length);
+            Task receiveTask = ReceiveMessageAsync();
+            Console.Write("Conected to server. To exit use /quit. For private message use /pm username and you message.\nEnter your message: ");
+            while (true)
+            {
+                try
+                {
+                    string message = Console.ReadLine();
+                    buffer = Encoding.UTF8.GetBytes(message);
+                    await stream.WriteAsync(buffer, 0, buffer.Length);
+                    if (message == "/quit")
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            client.Close();
+
+        }
+
+        static async Task ReceiveMessageAsync()
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                StringBuilder stringBuilder = new StringBuilder();
+                int bytesRead;
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    stringBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                    Console.WriteLine(stringBuilder.ToString());
+                    stringBuilder.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ np_hw1_server/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace np_hw1_server
+{
+
+
+    public class ChatServer
+    {
+        public List<Client> Clients = new List<Client>();
+        private TcpListener Listener { get; set; }
+        private int Port { get; set; }
+        public ChatServer(int port)
+        {
+            Port = port;
+        }
+        public async Task Listen()
+        {
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start();
+            Console.WriteLine("Server started and listening...");
+            while (true)
+            {
+                TcpClient tcpclient = await Listener.AcceptTcpClientAsync();
+                Client client = new Client { _Client = tcpclient, _Server = this  };
+                client.stream = tcpclient.GetStream();
+                Clients.Add(client);
+                client.HandleClientAsync();
+            }
+        }
+
+
+        public void BroadCastMessage(string message, Client client)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            foreach (var c in Clients)
+            {
+                if (c != client)
+                {
+                    client.stream.WriteAsync(buffer, 0, buffer.Length);
+                }
+            }
+        }
+        public void DeleteClient(Client client)
+        {
+            Clients.Remove(client);
+        }
+    }
+    public class Client
+    {
+        public TcpClient _Client { get; set; }
+        public string Username { get; set; }
+        public NetworkStream stream { get; set; }
+        public ChatServer _Server { get; set; }
+        public async void HandleClientAsync()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            Username = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"{Username} has joined");
+
+            try
+            {
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    if(message == "/quit")
+                    {
+                        Console.WriteLine($"{Username} has left the chat");
+                        _Server.DeleteClient(this);
+                        _Client.Close();
+                        break;
+                    }
+                    else if (message.StartsWith("/pm"))
+                    {
+                        string[] newMessage = message.Split(' ', 3);
+                        if(newMessage.Length >= 3)
+                        {
+                            string targetUser = newMessage[1];
+                            string pm = $"Private message from {Username}: " + newMessage[2];
+                            SendPM(targetUser, pm);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Username}: {message}");
+                        _Server.BroadCastMessage($"{Username}: {message}", this);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void SendPM(string targetUsername, string message)
+        {
+            Client targetClient = _Server.Clients.FirstOrDefault(u => u.Username == targetUsername);
+            if (targetClient != null)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                targetClient.stream.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
+    }
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ChatServer chat = new ChatServer(5000);
+            await chat.Listen();
+        }
+    }
+}
